d7a94369d114c68296c4f61822540229b1348da3
ShatoryElle <shator@gmail.com>
2026-06-08 14:39:07 +0300

    fix syntax error in config

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,13 @@
 import { defineConfig } from '@playwright/test';
 import 'dotenv/config';
+
 export default defineConfig({
-  
-use: {
-  baseURL: process.env.BASE_URL,
+  use: {
+    baseURL: process.env.BASE_URL || 'https://qauto.forstudy.space/',
 
-  httpCredentials: {
-   username: process.env.HTTP_USERNAME || 'guest'
-    password: process.env.HTTP_PASSWORD,
+    httpCredentials: {
+      username: process.env.HTTP_USERNAME || 'guest',
+      password: process.env.HTTP_PASSWORD || 'welcome2qauto',
+    },
   },
-},
-
 });
