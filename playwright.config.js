90361ad4262e7597f5d3a3735dea91d686ab0f18
ShatoryElle <shator@gmail.com>
2026-06-08 14:11:24 +0300

    fix CI env fallback

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from '@playwright/test';
 import 'dotenv/config';
 export default defineConfig({
-  
-use: {
-  baseURL: process.env.BASE_URL,
+  use: {
+    baseURL: 'https://qauto.forstudy.space/',
 
-  httpCredentials: {
-    username: process.env.HTTP_USERNAME,
-    password: process.env.HTTP_PASSWORD,
-  },
+    
+httpCredentials: {
+  username: process.env.HTTP_USERNAME || 'guest',
+  password: process.env.HTTP_PASSWORD || 'welcome2qauto',
 },
 
+  },
 });
